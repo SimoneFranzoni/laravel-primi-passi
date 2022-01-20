aea8b54833beeaba341961e78405f96a84017f95
SimoneFranzoni <simofranzoni99@gmail.com>
2022-01-20 17:21:25 +0100

    creazione progetto

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Contatti</title>
+</head>
+<body>
+    <div class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('about') }}">Chi siamo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('contatti') }}">Contatti</a>
+            </li>
+        </ul>
+        
+        <h1>Contatti</h1>
+    </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Laravel primi passi</title>
+</head>
+<body>
+    
+    <div class="container">
+    
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('about') }}">Chi siamo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('contatti') }}">Contatti</a>
+            </li>
+        </ul>
+
+        <h1>Home</h1> 
+        <p>Ciao {{ $name }}</p>   
+    </div>
+
+    
+
+
+</body>
+</html>
